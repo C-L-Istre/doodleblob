50adc2f737dea23737e66039068103870a840607
C.L.I. <121463895+C-L-Istre@users.noreply.github.com>
2026-06-13 14:47:13 -0500

    Update version_label.gd

--- scripts/version_label.gd
+++ scripts/version_label.gd
@@ -13,9 +13,15 @@ func _ready() -> void:
 	var minor := int(cfg.get_value("version", "minor", 0))
 	var patch := int(cfg.get_value("version", "patch", 0))
 
+	var commit := str(cfg.get_value("build", "commit", "local"))
 	var channel := str(cfg.get_value("channel", "name", "dev"))
 
+	# Base version string
 	text = "v%d.%d.%d" % [major, minor, patch]
 
+	# Add channel if not release
 	if channel != "release":
 		text += "-%s" % channel
+
+	# Add commit ALWAYS (or optionally only non-local builds)
+	text += "-%s" % commit
